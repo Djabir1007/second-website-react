9651648d01c9f471bd5498522d8001028b1ab1eb
Djabir1007 <jabir.pashaev@gmail.com>
2026-01-03 15:19:40 +0300

    добавление функционала

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -9,11 +10,4 @@ export default defineConfig({
       },
     }),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "./src/styles/base/global.scss" as *;`,
-      },
-    },
-  },
 });
