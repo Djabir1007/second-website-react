734a22e5f18d8798ebaed0aafa692a1d56374e83
Djabir1007 <jabir.pashaev@gmail.com>
2026-02-18 17:02:15 +0300

    удалил компонент wireless

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@
 import Hero from "@/components/Hero/Hero";
 import Covers from "@/components/Covers/Covers";
 import Headphones from "@/components/Headphones/Headphones";
-import Wireless from "@/components/Wireless/Wireless";
 
 // Types
 import type { Favorite, ToggleFavorite } from "@/types/favorite";
@@ -18,7 +17,6 @@ const Home = ({ favorites, toggleFavorite }: HomeProps) => {
       <Hero />
       <Covers />
       <Headphones favorites={favorites} toggleFavorite={toggleFavorite} />
-      <Wireless favorites={favorites} toggleFavorite={toggleFavorite} />
     </>
   );
 };
