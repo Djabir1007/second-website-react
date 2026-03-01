2b49dfb98e80e8446d366541706c689e8b998387
Djabir1007 <jabir.pashaev@gmail.com>
2026-03-01 17:41:07 +0300

    подключение React-Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/main.scss";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient(); // она будет нужна чтобы работать с react-query
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
 );
