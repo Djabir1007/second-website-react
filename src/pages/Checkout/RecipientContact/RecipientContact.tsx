6044cf0d0a65a8022c62d408a3345e8abe34e22d
Djabir1007 <jabir.pashaev@gmail.com>
2026-03-17 17:58:14 +0300

    добавил контролируемый инпут для номера получателя

--- src/pages/Checkout/RecipientContact/RecipientContact.tsx
+++ src/pages/Checkout/RecipientContact/RecipientContact.tsx
@@ -1,17 +1,18 @@
+import PhoneInput from "@/components/PhoneInput/PhoneInput";
+
 import styles from "./RecipientContact.module.scss";
 
 import { editIcon } from "@/assets/img";
+import { useState } from "react";
 
 const RecipientContact = () => {
+  const [phone, setPhone] = useState("");
+
   return (
     <section className={styles.recipientPhone}>
       <h3 className={styles.recipientPhoneTitle}>Номер получателя</h3>
       <div className={styles.recipientPhoneInfo}>
-        <input
-          className={styles.recipientPhoneValue}
-          type="tel"
-          placeholder="+7 ___ ___ ____"
-        />
+        <PhoneInput value={phone} onChange={setPhone} />
         <img className={styles.editIcon} src={editIcon} alt="Номер телефона" />
       </div>
     </section>
