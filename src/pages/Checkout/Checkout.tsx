be0b22aeb6f3e5356b98b5c252dfba068c55445f
Djabir1007 <jabir.pashaev@gmail.com>
2026-04-11 21:19:25 +0300

    добавил react hook form и валидацию в форму Checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,9 +1,12 @@
+import { useForm } from "react-hook-form";
+
 import DeliveryAddress from "./DeliveryAddress/DeliveryAddress";
 import OrderSummary from "./OrderSummary/OrderSummary";
 import PaymentSection from "./PaymentSection/PaymentSection";
 import RecipientContact from "./RecipientContact/RecipientContact";
 import CheckoutActions from "./CheckoutActions/CheckoutActions";
 import type { CartItem } from "@/types/cart";
+import type { CheckoutFormValues } from "@/types/checkoutForm";
 import styles from "./Checkout.module.scss";
 
 type CheckoutProps = {
@@ -11,17 +14,50 @@ type CheckoutProps = {
 };
 
 const Checkout = ({ cart }: CheckoutProps) => {
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    control,
+    formState: { errors },
+  } = useForm<CheckoutFormValues>({
+    defaultValues: {
+      city: "",
+      street: "",
+      house: "",
+      entrance: "",
+      apartment: "",
+      phone: "",
+      promocode: "",
+    },
+  });
+
+  const onSubmit = (data: CheckoutFormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
       <h2 className={styles.title}>Оформление заказа</h2>
       <section className={styles.checkout}>
-        <DeliveryAddress />
-        <aside className={styles.result}>
-          <OrderSummary cart={cart} />
-          <PaymentSection />
-          <RecipientContact />
-          <CheckoutActions />
-        </aside>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <DeliveryAddress
+            register={register}
+            setFocus={setFocus}
+            control={control}
+            errors={errors}
+          />
+          <aside className={styles.result}>
+            <OrderSummary cart={cart} />
+            <PaymentSection register={register} setFocus={setFocus} />
+            <RecipientContact
+              control={control}
+              setFocus={setFocus}
+              errors={errors}
+            />
+            <CheckoutActions />
+          </aside>
+        </form>
       </section>
     </>
   );
